7c991fd8f7187e86ca1b5508f16da3bb6f9c7534
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-11-19 00:26:33 +0600

    init

---
+++ lib/gradient_text.dart
@@ -0,0 +1,37 @@
+library gradient_text;
+
+import 'package:flutter/material.dart';
+import 'package:gradient_text/sample_colors.dart';
+
+class GradientText extends StatelessWidget {
+  const GradientText({
+    Key? key,
+    required this.text,
+    this.style,
+    this.gradient,
+  }) : super(key: key);
+  final String text;
+  final TextStyle? style;
+  final Gradient? gradient;
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      blendMode: BlendMode.srcIn,
+      shaderCallback: (bounds) => (gradient ??
+              const LinearGradient(
+                colors: [
+                  AppColors.gradient1,
+                  AppColors.gradient2,
+                  AppColors.gradient3,
+                ],
+                begin: Alignment(-1.4, -0.9),
+                end: Alignment(1.4, -0.9),
+              ))
+          .createShader(
+        Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+      ),
+      child: Text(text, style: style),
+    );
+  }
+}
---
+++ lib/sample_colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color gradient1 = Color(0xFF56B8FF);
+  static const Color gradient2 = Color(0xFF275AFF);
+  static const Color gradient3 = Color(0xFF9747FF);
+}
